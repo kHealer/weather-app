f8dcfc16262e4db876a3cd29f00010f9a3bb38a2
kHealer <helvacisefa@gmail.com>
2022-07-02 00:51:18 +0300

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd9cad37c25msh6b14fdfc67b0c63p1ec67djsn268efaa06126',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "995d95d7b2575438243f8079c8c9ef7f";
